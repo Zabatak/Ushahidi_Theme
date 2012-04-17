e8218371234f5a9b912479c13495038bd2d00e57
Abbas Ibrahim <abbas.adel@mufix.org>
2012-04-17 21:04:00 +0200

    add google analytics

--- views/header.php
+++ views/header.php
@@ -9,6 +9,20 @@
             // Action::header_scripts - Additional Inline Scripts from Plugins
             Event::run('ushahidi_action.header_scripts');
             ?>
+            <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-71480-8']);
+  _gaq.push(['_setDomainName', 'zabatak.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
     </head>
 
 
